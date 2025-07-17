ca6937f65b1ebba03e7dde76f8cfd4ddfd3923c3
Anatoliy Dudar <gigsollt@gmail.com>
2025-07-17 18:41:31 +0300

    finished transition to builder pattern

--- src/backend/color_converter.py
+++ src/backend/color_converter.py
@@ -1,15 +1,16 @@
+from typing import Self
 from numpy._typing import NDArray
 import cv2
 from PIL import Image
 import numpy as np
-from .models import ImportPalette, ImageData
+from .models import ImportPalette
 from .function_timer import function_timer
 from .palete_io import read_palete
 
 
 class ImagePipeline:
     @function_timer
-    def __init__(self, image_path: str, pallete_path: str) -> None:
+    def __init__(self, image_path: str) -> None:
         image = cv2.imread(image_path)
         if image is None:
             raise FileNotFoundError("Image is wrong")
@@ -18,26 +19,13 @@ class ImagePipeline:
         self.image: NDArray = image
 
     @function_timer
-    def quantize(self, n_clusters):
-        criteria = (
-            cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
-            10,
-            1.0,
-        )  # KMeans stops in case either 10 iterations happened or 100% quality labels acheaved
-        flags = cv2.KMEANS_RANDOM_CENTERS  # random starting points
-        z = np.float32(np.array(self.image_data.colors))
-        _, labels, centers = cv2.kmeans(
-            z,  # type: ignore
-            n_clusters,
-            None,  # type: ignore
-            criteria,
-            10,
-            flags,
-        )
+    def remap_to_existing_palette(self, palette_path: str) -> Self:
+        """
+        Selects unique colors and map them to a color from palette
+        based on euclidian distance between unique colors and palette
+        """
 
-    # TODO: Improve speed of color finding
-    @function_timer
-    def _get_unique_colors(self) -> tuple[ImportPalette, tuple]:
+        # convert image to a line of colors  so they are more faster to process
         colors_1d = self.image.reshape(-1, 3)
         tuple_line: tuple[tuple[int, ...], ...] = tuple(
             map(lambda row: (int(row[0]), int(row[1]), int(row[2])), colors_1d)
@@ -45,15 +33,12 @@ class ImagePipeline:
         unique_colors: ImportPalette = ImportPalette(
             name="unique_colors", colors=set(tuple_line)
         )
-        return unique_colors, tuple_line
 
-    @function_timer
-    def _map_palette_euclidean(
-        self, palette_path: str
-    ) -> tuple[dict[tuple[int, ...], tuple[int, ...]], tuple]:
+        # go through each color and map it using dict
         mapping: dict[tuple[int, ...], tuple[int, ...]] = {}
         palette = read_palete(palette_path)
-        unique_colors, tuple_line = self._get_unique_colors()
+
+        # find the closest color
         for color in unique_colors.colors:
             min_distance = float("inf")
             closest_color: tuple[int, ...] = (0, 0, 0)
@@ -70,28 +55,22 @@ class ImagePipeline:
 
             mapping[color] = closest_color
 
-        return mapping, tuple_line
-
-    @function_timer
-    def remap_to_existing_palette(self, palette_path: str) -> ImagePipeline:
-        mapping = self._map_palette_euclidean(palette_path)
-        for row in self.image:
-            for color in row:
-                row = mapping[color]
-
+        mapped = np.array([mapping[color]
+                          for color in tuple_line], dtype="uint8")
+        dim = self.image.shape
+        self.image = mapped.reshape(dim)
         return self
 
     @function_timer
-    def denoice(img: NDArray) -> NDArray:
-        new_image = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
-        return new_image
+    def denoice(self) -> Self:
+        new_image = cv2.fastNlMeansDenoisingColored(
+            self.image, None, 10, 10, 7, 21)
+        self.image = new_image
+        return self
 
 
 @function_timer
 def main(image_path: str, palette_path: str) -> None:
-    picture = read_image(image_path)
-    palette = read_palete(palette_path)
-    unique = get_unique_colors(picture)
-    mapping = map_palette_euclidean(unique, palette)
-    mapped_image = map_image(picture, mapping)
-    Image.fromarray(mapped_image).show()
+    image = ImagePipeline(image_path).denoice(
+    ).remap_to_existing_palette(palette_path)
+    Image.fromarray(image.image).show()
